25b6bac0ca8c2c79107378f0cc6712b292a5e99c
M Haidar Hanif <git@mhaidarhanif.com>
2023-07-29 22:04:26 +0700

    feat: setup tailwind and components

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,6 @@
 import type { LoaderArgs, ActionArgs, V2_MetaFunction } from "@remix-run/node";
 import { Form, useLoaderData } from "@remix-run/react";
+import { Button, Label, Input } from "~/components";
 import { prisma } from "~/db.server";
 
 export const meta: V2_MetaFunction = () => {
@@ -22,15 +23,15 @@ export default function Index() {
       <h1>Aldo Todo</h1>
 
       <Form method="POST">
-        <label htmlFor="text">Text:</label>
-        <input
+        <Label htmlFor="text">Text:</Label>
+        <Input
           type="text"
           id="text"
           name="text"
           placeholder="Enter your todo..."
         />
 
-        <button type="submit">Add Todo</button>
+        <Button type="submit">Add Todo</Button>
       </Form>
 
       <ul>
